65a46bca5249ac7b548ae4251dd972e3e18ba0a0
Anastasiia Lapshina <91057358+lapshinaaa@users.noreply.github.com>
2023-02-21 13:38:29 +0300

    update header

--- header.h
+++ header.h
@@ -11,6 +11,9 @@ template <typename Type>
 std::vector<Type> replacingOddEven(std::vector<Type>&vector, std::list<Type>&list);
 
 template<typename T>
+std::vector<T> print_even(std::list<T>&lst);
+
+template<typename T>
 std::list<T> alternate_pairs(const std::vector<T>& vec, const std::list<T>& lst);
 
 void task4();
