bbb020074ddbf61233c91350f016f6e5c1503a0e
Anastasiia Lapshina <91057358+lapshinaaa@users.noreply.github.com>
2023-02-21 13:45:33 +0300

    update task

--- task 4.cpp
+++ task 4.cpp
@@ -1,10 +1,27 @@
+template<typename T>
+std::vector<T> creatingEvenVector(std::list<T>&lst)
+{
+    std::vector<int> vector_with_evens;
+    for (int num : lst) {
+        if (num % 2 == 0) vector_with_evens.push_back(num);
+    }
+
+    std::cout << "Vector with even elements is : ";
+    for (int num : vector_with_evens) {
+        std::cout << num << " ";
+    }
+
+    std::cout<<"\n";
+    return vector_with_evens;
+}
+
 void task4() {
 
     int size_of_list = 0;
     std::cout << "Enter the size of your list: ";
     std::cin >> size_of_list;
+    std::list<int> list_of_digits;
 
-    std::list <int> list_of_digits;
     int elem = 0;
 
     for (int i = 0; i < size_of_list; ++i) //   filling in the list
@@ -13,18 +30,5 @@ void task4() {
         list_of_digits.push_back(elem);
     }
 
-    std::vector <int> vector_of_evens;
-
-    for (int num : list_of_digits) {
-        if (num % 2 == 0) {
-            vector_of_evens.push_back(num);
-        }
-    }
-
-    std::cout << "Even elements are: ";
-    for (int num : vector_of_evens) {
-        std::cout << num << " ";
-    }
-
-    std::cout << std::endl;
+    creatingEvenVector(list_of_digits);
 }
